6276a5af971378192403ecb608302469ba6b72b8
Mike Arthur <mike@mikearthur.co.uk>
2010-01-25 18:22:11 +0000

    Initial commit.

---
+++ Fabula.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2010-01-25T18:12:04
+#
+#-------------------------------------------------
+
+QT       += xml phonon
+
+TARGET = Fabula
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        MainWindow.cpp
+
+HEADERS  += MainWindow.h
+
+FORMS    += MainWindow.ui
---
+++ MainWindow.cpp
@@ -0,0 +1,26 @@
+#include "MainWindow.h"
+#include "ui_MainWindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::changeEvent(QEvent *e)
+{
+    QMainWindow::changeEvent(e);
+    switch (e->type()) {
+    case QEvent::LanguageChange:
+        ui->retranslateUi(this);
+        break;
+    default:
+        break;
+    }
+}
