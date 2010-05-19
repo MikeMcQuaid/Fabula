6b8aa3b5b7215c2bd07555845ff230fa317839a3
Mike Arthur <mike@mikearthur.co.uk>
2010-05-19 14:00:32 +0200

    Add GPLv3 license information and copyright.

--- TableTreeModel.cpp
+++ TableTreeModel.cpp
@@ -1,3 +1,20 @@
+/*  This file is part of Fabula.
+    Copyright (C) 2010 Mike Arthur <mike@mikearthur.co.uk>
+
+    Fabula is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Fabula is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Fabula. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "TableTreeModel.h"
 
 #include <QStringList>
