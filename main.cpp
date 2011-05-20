4d2d1803e922dc1315520fda4325deebc2602551
Mike McQuaid <mike@mikemcquaid.com>
2011-05-20 17:31:51 +0100

    Fix Windows window icon failure.

--- main.cpp
+++ main.cpp
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 #ifdef Q_WS_MAC
     application.setAttribute(Qt::AA_DontShowIconsInMenus);
 #else
-    application.setWindowIcon(":/icons/audio-input-microphone.png");
+    application.setWindowIcon(QIcon(":/icons/audio-input-microphone.png"));
 #endif
 
     MainWindow mainWindow;
